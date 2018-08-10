f52f6add900b56e9eabca01364374ea96f59e1cc
README <40898615+AlexZhuang2018@users.noreply.github.com>
2018-08-10 23:18:38 +0800

    Create service_connect.rb

---
+++ B2/models/service_connect.rb
@@ -0,0 +1,7 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'mysql2',
+                                        host: 'localhost',
+                                        database: 'web_tasks',
+                                        username: 'root',
+                                        password: '')
